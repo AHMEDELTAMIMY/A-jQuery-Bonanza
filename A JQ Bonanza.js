5ee58ec4df4dadc95c05869b766db3af6ed391f2
AHMED ELTAMIMY <79113610+AHMEDELTAMIMY@users.noreply.github.com>
2021-03-21 13:35:30 +0100

    First Commit

---
+++ A JQ Bonanza.js
@@ -0,0 +1,48 @@
+// $('#btn1').click(function(){
+//   alert('Button clicked');
+//   });
+
+// $('.para').text('Hello World');
+// $('.para').html('<h3>Hello World</h3>');
+
+// $('h1').hide();
+// $('h1').show();
+
+
+// $('#btn2').on('click',function(){
+//     $('.para1').show();
+
+
+// $('p.para2').addClass('myClass');
+// $( "p" ).last().addClass( "selected highlight" );
+
+// $('ul').append('<li> Append List Item </li>');
+// $('ul').prepend('<li> Prepend List Item </li>');
+
+
+// $( "#clickme" ).click(function() {
+//   $( "#book" ).fadeIn( "slow", function() {
+//   });
+// });  
+
+// $( document.fadin ).click(function() {
+//   $( "div:hidden" ).first().fadeIn( "slow" );
+// });
+
+$( ".fadin" ).click(function() {
+  $( ".fadin" ).fadeOut( "slow" );
+}); 
+
+/* $( "input" )
+  .keyup(function() {
+    var value = $( this ).val();
+    $( "p" ).text( value );
+  }) */
+
+// $( document.body ).click(function () {
+//   if ( $( "div" ).first().is( ":hidden" ) ) {
+//     $( "div" ).slideDown( "slow" );
+//   } else {
+//     $( "div" ).hide();
+//   }
+// }); */}
